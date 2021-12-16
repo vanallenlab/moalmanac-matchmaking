3156f5edff3c792ccb6e9b3b89c3afcce9c97dfa
Brendan Reardon <breardon@broadinstitute.org>
2021-12-15 20:39:08 -0500

    Revised annotate molecular features script to have default values for outputs

--- matchmaking/inputs/annotated/annotate-molecular-features.sh
+++ matchmaking/inputs/annotated/annotate-molecular-features.sh
@@ -5,6 +5,9 @@ MOALMANAC_VENV=$2
 INPUT_SOMATIC=$3
 INPUT_COPY_NUMBER=$4
 INPUT_FUSION=$5
+OUTPUT_SOMATIC=${6-samples.variants.annotated.testing.txt}
+OUTPUT_COPY_NUMBER=${7-samples.copy_numbers.annotated.testing.txt}
+OUTPUT_FUSION=${8-samples.fusions.annotated.testing.txt}
 WD=$PWD
 
 cp annotate-variants.py annotate-copy-numbers.py annotate-fusions.py "$MOALMANAC_DIR"
@@ -13,8 +16,9 @@ cp "$INPUT_COPY_NUMBER" "$MOALMANAC_DIR"/matchmaking-copy-number-alterations.txt
 cp "$INPUT_FUSION" "$MOALMANAC_DIR"/matchmaking-fusions.txt
 cd "$MOALMANAC_DIR" || exit
 
-$MOALMANAC_VENV annotate-variants.py --input matchmaking-somatic-variants.txt --output samples.variants.annotated.txt --directory "$WD"
-$MOALMANAC_VENV annotate-copy-numbers.py --input matchmaking-copy-number-alterations.txt --output samples.copy-number.annotated.txt --directory "$WD" --directory "$WD"
-$MOALMANAC_VENV annotate-fusions.py --input matchmaking-fusions.txt --output samples.fusions.annotated.txt --directory "$WD" --directory "$WD"
+$MOALMANAC_VENV annotate-variants.py --input matchmaking-somatic-variants.txt --output "$OUTPUT_SOMATIC" --directory "$WD"
+$MOALMANAC_VENV annotate-copy-numbers.py --input matchmaking-copy-number-alterations.txt --output "$OUTPUT_COPY_NUMBER" --directory "$WD"
+$MOALMANAC_VENV annotate-fusions.py --input matchmaking-fusions.txt --output "$OUTPUT_FUSION" --directory "$WD"
 
-rm annotate-variants.py annotate-copy-numbers.py annotate-fusions.py
+rm "$MOALMANAC_DIR"/annotate-variants.py "$MOALMANAC_DIR"/annotate-copy-numbers.py "$MOALMANAC_DIR"/annotate-fusions.py
+rm "$MOALMANAC_DIR"/matchmaking-somatic-variants.txt "$MOALMANAC_DIR"/matchmaking-copy-number-alterations.txt "$MOALMANAC_DIR"/matchmaking-fusions.txt
